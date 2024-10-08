fcc98301c266e81e6cf467b7a2265e9e1a4d7e5b
nikita-radion <65399810+nikita-radion@users.noreply.github.com>
2024-10-08 11:08:17 +0300

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -1,11 +1,28 @@
 #include <gtest/gtest.h>
 #include "polindrom.h"
 
-TEST(EliminateUnsetBitsTest, BasicTests) {
-    EXPECT_EQ(check_polindrom("polindrom"), 0);
-    EXPECT_EQ(check_polindrom("111"), 1);
-    EXPECT_EQ(check_polindrom("madam"), 1);
-    EXPECT_EQ(check_polindrom("level"), 1);
+TEST(PolindromTest, Polindrom) {
+    std::string input = "polindrom";
+    int result = check_polindrom(input);
+    EXPECT_EQ(result, 0);
+}
+
+TEST(PolindromTest, Numeric) {
+    std::string input = "111";
+    int result = check_polindrom(input);
+    EXPECT_EQ(result, 1);
+}
+
+TEST(PolindromTest, Madam) {
+    std::string input = "madam";
+    int result = check_polindrom(input);
+    EXPECT_EQ(result, 1);
+}
+
+TEST(PolindromTest, Level) {
+    std::string input = "level";
+    int result = check_polindrom(input);
+    EXPECT_EQ(result, 1);
 }
 
 int main(int argc, char **argv) {
